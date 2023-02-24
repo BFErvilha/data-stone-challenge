d47d6ef806903ddb18c60633aa8e54ae13821533
Bruno Ervilha <bruno.ervilha@landapp.com.br>
2023-02-24 09:27:18 -0300

    update product

--- src/views/Products/Product.vue
+++ src/views/Products/Product.vue
@@ -1,33 +1,37 @@
 <template>
-  <b-row>
-    <b-col cols="12"> Novo Produto </b-col>
-    <b-col>
-      <b-card style="max-width: 20rem; margin: 0 auto" class="mb-2">
-        <b-form-group id="fieldset-1" label="Produto" label-for="input-1">
-          <b-form-input
-            id="input-1"
-            v-model="product.name"
-            :state="state"
-            trim
-          ></b-form-input>
-        </b-form-group>
+  <b-container>
+    <b-row>
+      <b-col cols="12"> Novo Produto </b-col>
+      <b-col cols="12">
+        <b-card style="max-width: 20rem; margin: 0 auto" class="mb-2">
+          <b-form-group id="fieldset-1" label="Produto" label-for="input-1">
+            <b-form-input
+              id="input-1"
+              v-model="product.name"
+              trim
+            ></b-form-input>
+          </b-form-group>
 
-        <b-form-group id="fieldset-1" label="Status" label-for="input-1">
-          <b-form-checkbox
-            id="checkbox-1"
-            v-model="product.status"
-            name="checkbox-1"
-            value="accepted"
-            unchecked-value="not_accepted"
+          <b-form-group id="fieldset-1" label="Status" label-for="input-1">
+            <b-form-checkbox
+              id="checkbox-1"
+              v-model="product.status"
+              name="checkbox-1"
+            >
+              Ativo
+            </b-form-checkbox>
+          </b-form-group>
+
+          <b-button v-if="!productId" @click="saveProduct()" variant="primary"
+            >Registrar</b-button
           >
-            Ativo
-          </b-form-checkbox>
-        </b-form-group>
-
-        <b-button @click="saveProduct()" variant="primary">Registrar</b-button>
-      </b-card>
-    </b-col>
-  </b-row>
+          <b-button v-else @click="updateProduct()" variant="primary"
+            >Atualizar</b-button
+          >
+        </b-card>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 <script>
 export default {
@@ -35,6 +39,7 @@ export default {
   name: 'Product',
   data() {
     return {
+      productId: null,
       product: {
         name: null,
         status: false,
@@ -46,17 +51,25 @@ export default {
       this.$store.dispatch('addNewProduct', this.product);
       this.$router.push({ name: 'productsList' });
     },
+    updateProduct() {
+      this.product.id = this.productId;
+      this.$store.dispatch('updateExistingProduct', this.product);
+      this.$router.push({ name: 'productsList' });
+    },
     getStoreProduct(productId) {
       const allProducts = this.$store.getters.allProducts;
-      this.product = allProducts.filter(
-        (product) => product.id === parseInt(productId),
-      );
+      allProducts.map((item) => {
+        if (item.id === productId) {
+          this.product.name = item.name;
+          this.product.status = item.status;
+        }
+      });
     },
   },
   created() {
     if (this.$route.params.productId) {
-      const productId = parseInt(this.$route.params.productId);
-      this.getStoreProduct(productId);
+      this.productId = parseInt(this.$route.params.productId);
+      this.getStoreProduct(this.productId);
     }
   },
 };
